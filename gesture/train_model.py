14fbcd6971014a2d194c14f02959e4665ac145fd
BlackCodeReaper <davidecrive03@gmail.com>
2025-06-30 23:49:46 +0200

    fixes model

--- gesture/train_model.py
+++ gesture/train_model.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 import numpy as np
 import joblib
 import os
@@ -20,22 +20,30 @@ def main():
     X = df.drop("label", axis=1)
     y = df["label"]
 
+    # Divisione train/test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
     # Modello Random Forest
     clf = RandomForestClassifier(n_estimators=100, random_state=42)
     cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-    scores = cross_val_score(clf, X, y, cv=cv)
+    scores = cross_val_score(clf, X_train, y_train, cv=cv)
 
     print("\n== REPORT ==")
     print("Accuracy per ogni fold:", scores)
     print("Media:", np.mean(scores))
     print("Deviazione standard:", np.std(scores))
 
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    print("Accuracy sul test set:", model.score(X_test, y_test))
+
     # Salvataggio modello
     if not os.path.exists("model"):
         os.makedirs("model")
 
-    joblib.dump(clf, MODEL_PATH)
+    joblib.dump(model, MODEL_PATH)
     print(f"\n[✓] Modello salvato in {MODEL_PATH}")
 
 if __name__ == "__main__":
